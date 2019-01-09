d639f7e3c7cba57c68bbb192e430fbdc80390f84
Crash <hex22a@gmail.com>
2019-01-10 01:10:39 +0300

    Crate first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import 'normalize.css';
 
+import NameBox from './components/NameBox/NameBox';
+
 class App extends Component {
   render() {
     return (
       <div>
         Ohi
+        <NameBox/>
       </div>
     );
   }
---
+++ src/components/NameBox/NameBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NameBox extends Component {
+  render() {
+    return (
+      <div>Lil Pimp</div>
+    );
+  }
+}
+
+export default NameBox;
