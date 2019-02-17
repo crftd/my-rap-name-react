37517a54b0d4da83e94df544e11c90ce5eba1b7e
Crash <hex22a@gmail.com>
2019-02-17 17:20:42 +0300

    Pretty code

--- src/services/rap-name.js
+++ src/services/rap-name.js
@@ -1,14 +1,7 @@
-export const RAP_NAMES = [
-  'SQWOZ BAB',
-  'Lil pump',
-  'XXXTENTACTION',
-  'FACE',
-  'Pasha Technique',
-  'Big Baby Tape',
-];
+export const RAP_NAMES = ['SQWOZ BAB', 'Lil pump', 'XXXTENTACTION', 'FACE', 'Pasha Technique', 'Big Baby Tape'];
 
-export const getRapName = () =>
-  RAP_NAMES[functions.getRandomInt(RAP_NAMES.length)];
+export const getRapName = () => RAP_NAMES[functions.getRandomInt(RAP_NAMES.length)];
+
 export const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 export const functions = { getRandomInt };
