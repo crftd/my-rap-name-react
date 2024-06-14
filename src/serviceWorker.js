45254a4adc5672d25fc2a98889fb018db888b222
Crash <1903291+hex22a@users.noreply.github.com>
2024-06-14 16:16:56 +0400

    Made it start again
    Update Cypress configuration and refactor related files
    
    Deleted old Cypress files and added updated test files along with a new configuration. Also updated dependencies in package.json including the major version of Cypress. Furthermore, the message format in serviceWorker.js has been slightly altered, and some syntax corrections have been made.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,13 +11,11 @@
 // opt-in, read http://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost'
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    || window.location.hostname === '[::1]'
+    window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    || window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
 export function register(config) {
@@ -42,8 +40,8 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.info(
-            'This web app is being served cache-first by a service '
-              + 'worker. To learn more, visit http://bit.ly/CRA-PWA'
+            'This web app is being served cache-first by a service ' +
+              'worker. To learn more, visit http://bit.ly/CRA-PWA',
           );
         });
       } else {
@@ -70,8 +68,8 @@ function registerValidSW(swUrl, config) {
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               console.info(
-                'New content is available and will be used when all '
-                  + 'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+                'New content is available and will be used when all ' +
+                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.',
               );
 
               // Execute callback
@@ -104,10 +102,7 @@ function checkValidServiceWorker(swUrl, config) {
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404
-        || (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
+      if (response.status === 404 || (contentType != null && contentType.indexOf('javascript') === -1)) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
@@ -120,9 +115,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.error(
-        'No internet connection found. App is running in offline mode.'
-      );
+      console.error('No internet connection found. App is running in offline mode.');
     });
 }
 
